1a13096f9f3d4b1017a305e4290c3b259260bb38
Frid Matilda <matilda.x.frid@scania.com>
2023-05-31 22:32:39 +0200

    fix

--- src/App.js
+++ src/App.js
@@ -1,69 +1,29 @@
-import React, { useState } from 'react';
-import { API_KEY, BASE_URL } from 'utils/urls';
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Explore } from './components/Explore'
+import { StartPage } from './components/StartPage'
+// import { LogIn } from './components/LogIn'
+// import { Register } from './components/Register'
+// import { About } from './components/About'
+// import { Profile } from './components/Profile'
+// import { Trip } from './components/Trip'
+// import { NotFound } from './components/NotFound'
 
 export const App = () => {
-  const [inputLong, setInputLong] = useState(null);
-  const [inputLat, setInputLat] = useState(null);
-  const [input, setInput] = useState('');
-  const [placesData, setPlacesData] = useState([]);
-
-  const geoUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${input}&key=${API_KEY}`;
-
-  const fetchData = () => {
-    fetch(geoUrl)
-      .then((response) => {
-        if (response.ok) {
-          return response.json();
-        } else {
-          throw new Error(`HTTP error! Status: ${response.status}`);
-        }
-      })
-      .then((data) => {
-        const { lng, lat } = data.results[0].geometry.location;
-        setInputLong(lng);
-        setInputLat(lat);
-        console.log(data.results)
-
-        fetch(`${BASE_URL}`, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({ lng, lat })
-        })
-          .then((response) => response.json())
-          .then((json) => {
-            console.log(json);
-            setPlacesData(json.results);
-          })
-          .catch((error) => console.error('Error:', error));
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  };
-
-  const handleFormSubmit = (event) => {
-    event.preventDefault();
-    fetchData();
-  };
-
   return (
-    <>
-      <form action="" onSubmit={handleFormSubmit}>
-        <input type="text" value={input} onChange={(event) => setInput(event.target.value)} />
-      </form>
-      <p>{input} has the coordinates: long {inputLong}, lat {inputLat}</p>
-
-      {placesData && placesData.map((places) => (
-        // eslint-disable-next-line no-underscore-dangle
-        <div key={places.place_id}>
-          <h2>{places.name}</h2>
-          <p>⭐️{places.rating}</p>
-          <img src={places.geometry.icon} alt="" />
-          <img src={places.photos[0].photo_reference} alt="" />
-        </div>
-      ))}
-    </>
-  );
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<StartPage />} />
+        <Route path="/explore" element={<Explore />} />
+        {/*
+            <Route path="/login" element={<LogIn />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/trip" element={<Trip />} />
+            <Route path="*" element={<NotFound />} />
+        */}
+      </Routes>
+    </BrowserRouter>
+  )
 }
