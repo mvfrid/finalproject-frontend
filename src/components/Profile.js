4c48c76c96efdfd8ac9abb70711fd964286a0810
Frid Matilda <matilda.x.frid@scania.com>
2023-06-01 16:58:42 +0200

    added profile placeholders

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,9 +1,44 @@
 import React from 'react';
+// import React, { useEffect, useState } from 'react';
+// import { useDispatch, useSelector } from 'react-redux';
+// import { user } from 'reducers/user';
 
 export const Profile = () => {
   return (
     <div className="main">
-      <h1>Profile</h1>
+      <div className="profile-section">
+        <h1>Profile</h1>
+        <p>username</p>
+      </div>
+      <div className="trip-section">
+        <h2>Trip collections:</h2>
+        <div className="trip-wrapper">
+          <div className="trip">
+            <h3>Trip 1</h3>
+            <p>Content here, replace with cards</p>
+          </div>
+          <div className="trip">
+            <h3>Trip 2</h3>
+            <p>Content here, replace with cards</p>
+          </div>
+          <div className="trip">
+            <h3>Trip 3</h3>
+            <p>Content here, replace with cards</p>
+          </div>
+          <div className="trip">
+            <h3>Trip 4</h3>
+            <p>Content here, replace with cards</p>
+          </div>
+          <div className="trip">
+            <h3>Trip 5</h3>
+            <p>Content here, replace with cards</p>
+          </div>
+          <div className="trip">
+            <h3>Trip 6</h3>
+            <p>Content here, replace with cards</p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
