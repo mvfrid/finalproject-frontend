bd742f6d15d8fc4eab22d250ee61de486429c0e2
Frid Matilda <matilda.x.frid@scania.com>
2023-06-04 22:59:17 +0200

    moved the store one level above App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,6 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { user } from 'reducers/user';
-import { trip } from 'reducers/trip';
-import { card } from 'reducers/card';
-import { combineReducers, configureStore } from '@reduxjs/toolkit';
-import { Provider } from 'react-redux';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import { Header } from './components/Header'
 import { Explore } from './components/Explore'
 import { StartPage } from './components/StartPage'
@@ -12,7 +8,39 @@ import { LogInRegister } from './components/LogInRegister'
 import { NotFound } from './components/NotFound'
 import { About } from './components/About'
 import { Profile } from './components/Profile/Profile'
-// import { Trip } from './components/Trip'
+
+export const App = () => {
+  const accessToken = useSelector((state) => state.user.accessToken);
+
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<StartPage />} />
+        {accessToken !== null ? (
+          <Route path="/explore" element={<Explore />} />
+        ) : (
+          <Route
+            path="/explore"
+            element={<Navigate to="/login" replace />} />
+        )}
+        <Route path="/login" element={<LogInRegister mode="/users/login" />} />
+        <Route path="/register" element={<LogInRegister mode="/users/register" />} />
+        <Route path="/about" element={<About />} />
+        {accessToken !== null ? (
+          <Route path="/profile" element={<Profile />} />
+        ) : (
+          <Route
+            path="/profile"
+            element={<Navigate to="/login" replace />} />
+        )}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+/*
 
 export const App = () => {
   const reducer = combineReducers({
@@ -23,43 +51,36 @@ export const App = () => {
 
   const store = configureStore({ reducer })
 
+  const accessToken = useSelector((state) => state.user.accessToken);
+
   return (
     <Provider store={store}>
       <BrowserRouter>
         <Header />
         <Routes>
           <Route path="/" element={<StartPage />} />
-          <Route path="/explore" element={<Explore />} />
+          {accessToken !== null ? (
+            <Route path="/explore" element={<Explore />} />
+          ) : (
+            <Route
+              path="/explore"
+              element={<Navigate to="/login" replace />} />
+          )}
           <Route path="/login" element={<LogInRegister mode="/users/login" />} />
           <Route path="/register" element={<LogInRegister mode="/users/register" />} />
           <Route path="/about" element={<About />} />
-          <Route path="/profile" element={<Profile />} />
+          {accessToken !== null ? (
+            <Route path="/profile" element={<Profile />} />
+          ) : (
+            <Route
+              path="/profile"
+              element={<Navigate to="/login" replace />} />
+          )}
           <Route path="*" element={<NotFound />} />
-
-          {/*
-            <Route path="/trip" element={<Trip />} />
-        */}
-
         </Routes>
       </BrowserRouter>
     </Provider>
   )
 }
 
-/*
-MATILDA TESTAR, FUNKAR EJ :(
-
-  const reducer = combineReducers({
-    user: user.reducer,
-    trip: trip.reducer,
-    card: card.reducer
-  });
-
-  const accessToken = useSelector((state) => state.user.accessToken);
-
-          {accessToken !== null ? (
-            <Route path="/profile" element={<Profile />} />
-          ) : (
-            <Route path="/notauthorized" element={<LogInRegister />} />
-          )}
- */
+*/
--- src/index.js
+++ src/index.js
@@ -1,8 +1,35 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
+import { user } from 'reducers/user';
+import { trip } from 'reducers/trip';
+import { card } from 'reducers/card';
+import { combineReducers, configureStore } from '@reduxjs/toolkit';
+import { Provider } from 'react-redux';
 import { App } from './App';
+
+const reducer = combineReducers({
+  user: user.reducer,
+  trip: trip.reducer,
+  card: card.reducer
+});
+
+const store = configureStore({ reducer })
 
 const container = document.getElementById('root');
 const root = createRoot(container);
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
+/*
+  const reducer = combineReducers({
+    user: user.reducer,
+    trip: trip.reducer,
+    card: card.reducer
+  });
+
+  const store = configureStore({ reducer })
+  */
