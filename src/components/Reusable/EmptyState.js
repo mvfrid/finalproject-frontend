3a9fe63eec52e48227dc849ec85ce77c662a9342
Frid Matilda <matilda.x.frid@scania.com>
2023-06-06 12:27:02 +0200

    moved the search field into its own component, saved a backup file

--- src/components/Reusable/EmptyState.js
+++ src/components/Reusable/EmptyState.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const EmptyState = () => {
   return (
-    <div className="main">
+    <div className="placeholder">
       <h1>Empty state</h1>
     </div>
   )
