e9a81cbb62fce0dfa55c3288580fda1cf832e6e4
camcron <camillacronqvist@hotmail.com>
2023-06-11 17:42:01 +0200

    added styling for startpage

--- src/components/Startpage/StartPage.js
+++ src/components/Startpage/StartPage.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import './StartPage.css';
+import { Player } from '@lottiefiles/react-lottie-player';
+import { Button } from '@mui/material';
 
 export const StartPage = () => {
   const navigate = useNavigate();
@@ -9,10 +11,47 @@ export const StartPage = () => {
     navigate('/login');
   }
 
+  const onClickGoToRegister = () => {
+    navigate('/register');
+  }
+
   return (
-    <div className="main">
-      <h1>Start Page</h1>
-      <button type="button" onClick={onClickGoToLogin}>Go to Log In</button>
-    </div>
+    <>
+      <h2 className="start-h2">Save your travel inspo</h2>
+      <div className="empty-container">
+        <Player
+          src="https://assets10.lottiefiles.com/packages/lf20_2w54jzgb.json"
+          loop
+          autoplay
+          style={{
+            width: '300px'
+          }} />
+      </div>
+      <div className="button-container">
+        <Button
+          sx={{
+            backgroundColor: '#446173',
+            '&:hover': {
+              backgroundColor: '#345461' // Optional: You can also control the hover color here
+            }
+          }}
+          variant="contained"
+          type="button"
+          onClick={onClickGoToLogin}>
+      Go to Log In
+        </Button>
+        <Button
+          sx={{
+            color: '#446173',
+            '&:hover': {
+              color: '#345461' // Optional: You can also control the hover color here
+            }
+          }}
+          type="button"
+          onClick={onClickGoToRegister}>
+      Not a member yet? Go to register
+        </Button>
+      </div>
+    </>
   )
 }
