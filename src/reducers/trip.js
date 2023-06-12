461d5ce5562e81d3c093f942bd5ef0fa61bedf85
camcron <camillacronqvist@hotmail.com>
2023-06-12 20:58:47 +0200

    fixed a bug causing reviewvalue to disappear

--- src/reducers/trip.js
+++ src/reducers/trip.js
@@ -240,7 +240,7 @@ export const deleteTrip = (tripId) => {
           // const responseData = response.response.data;
           // console.log('responseData:', responseData);
           dispatch(trip.actions.setDeleteTrip(tripId));
-          fetchTrips()
+          dispatch(fetchTrips());
         } else {
           dispatch(trip.actions.setError(response));
         }
