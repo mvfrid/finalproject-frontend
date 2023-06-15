f4431cc00580fc1f2597d8d4e78fe3473045cce9
Frid Matilda <matilda.x.frid@scania.com>
2023-06-15 09:41:46 +0200

    styled empty state

--- src/components/Explore/SingleCardPreviewExplore/SingleCardPreviewExplore.js
+++ src/components/Explore/SingleCardPreviewExplore/SingleCardPreviewExplore.js
@@ -1,3 +1,4 @@
+/* eslint-disable linebreak-style */
 import React from 'react';
 import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
--- src/components/HeadNavFoot/Header/Header.js
+++ src/components/HeadNavFoot/Header/Header.js
@@ -1,3 +1,4 @@
+/* eslint-disable linebreak-style */
 /* eslint-disable no-unused-vars */
 /* eslint-disable max-len */
 import React, { useEffect, useState } from 'react';
