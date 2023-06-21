7296eec01373942871a6b88e5f91b6aec15862d8
Frid Matilda <matilda.x.frid@scania.com>
2023-06-21 22:53:58 +0200

    added badge to show that the card has comments

--- src/components/SingleTrip/SingleTripCardPreview/SingleTripCardPreview.js
+++ src/components/SingleTrip/SingleTripCardPreview/SingleTripCardPreview.js
@@ -1,4 +1,6 @@
 /* eslint-disable linebreak-style */
+/* eslint-disable react/jsx-no-useless-fragment */
+/* eslint-disable no-unused-vars */
 /* eslint-disable react/jsx-closing-tag-location */
 import React from 'react';
 import Card from '@mui/material/Card';
@@ -7,6 +9,7 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import Badge from '@mui/material/Badge';
 import { API_KEY } from 'utils/urls';
 import * as styles from './StyledSingleTripCardPreview.js'
 
@@ -15,36 +18,46 @@ export const SingleTripCardPreview = ({ card, showButton, onCardClick }) => {
     onCardClick(card);
   }
 
+  console.log('card:', card)
+
   const photoWidth = 500;
   const photoReference = card.cardPhotoRef
   const placeholderImg = 'https://i.postimg.cc/c4zXpFPD/thomas-kinto-6-Ms-MKWz-JWKc-unsplash.jpg';
   const photoUrl = `https://maps.googleapis.com/maps/api/place/photo?maxwidth=${photoWidth}&photoreference=${photoReference}&key=${API_KEY}`
 
   return (
-    <Card
-      key={card.place_id}
-      sx={styles.StyledPreviewContainer}>
-      <CardMedia
-        sx={styles.StyledCardPreviewImg}
-        image={photoReference ? photoUrl : placeholderImg} />
-      <CardContent
-        sx={styles.StyledCardContentSingle}>
-        <Typography
-          sx={styles.StyledCardTypoName}
-          gutterBottom
-          variant="h5"
-          component="div">
-          {card.cardName}
-        </Typography>
-        <Typography
-          sx={styles.StyledCardTypoRat}
-          variant="body2">
-          <span role="img" aria-label="Rating">⭐️</span>
-          {card.cardRating}
-        </Typography>
-      </CardContent>
-      <CardActions>
-        {showButton
+    <>
+      {card.cardComment ? (
+        <Badge
+          badgeContent="+"
+          color="primary"
+          sx={{ fontSize: '43px' }}
+          anchorOrigin={{
+            vertical: 'top',
+            horizontal: 'right'
+          }}>
+          <Card key={card.place_id} sx={styles.StyledPreviewContainer}>
+            <CardMedia
+              sx={styles.StyledCardPreviewImg}
+              image={photoReference ? photoUrl : placeholderImg} />
+            <CardContent
+              sx={styles.StyledCardContentSingle}>
+              <Typography
+                sx={styles.StyledCardTypoName}
+                gutterBottom
+                variant="h5"
+                component="div">
+                {card.cardName}
+              </Typography>
+              <Typography
+                sx={styles.StyledCardTypoRat}
+                variant="body2">
+                <span role="img" aria-label="Rating">⭐️</span>
+                {card.cardRating}
+              </Typography>
+            </CardContent>
+            <CardActions>
+              {showButton
         && <Button
           sx={styles.StyledCardBtn}
           size="small"
@@ -52,7 +65,42 @@ export const SingleTripCardPreview = ({ card, showButton, onCardClick }) => {
           onClick={handleOpenModal}>
           Read More
         </Button>}
-      </CardActions>
-    </Card>
+            </CardActions>
+          </Card>
+        </Badge>
+      ) : (
+        <Card key={card.place_id} sx={styles.StyledPreviewContainer}>
+          <CardMedia
+            sx={styles.StyledCardPreviewImg}
+            image={photoReference ? photoUrl : placeholderImg} />
+          <CardContent
+            sx={styles.StyledCardContentSingle}>
+            <Typography
+              sx={styles.StyledCardTypoName}
+              gutterBottom
+              variant="h5"
+              component="div">
+              {card.cardName}
+            </Typography>
+            <Typography
+              sx={styles.StyledCardTypoRat}
+              variant="body2">
+              <span role="img" aria-label="Rating">⭐️</span>
+              {card.cardRating}
+            </Typography>
+          </CardContent>
+          <CardActions>
+            {showButton
+        && <Button
+          sx={styles.StyledCardBtn}
+          size="small"
+          variant="outlined"
+          onClick={handleOpenModal}>
+          Read More
+        </Button>}
+          </CardActions>
+        </Card>
+      )}
+    </>
   );
 }
